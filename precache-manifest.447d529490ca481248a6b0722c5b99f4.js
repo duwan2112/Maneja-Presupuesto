940ea4535885d0c0ff691768cccc0d0dfe4fe3fb
Duwan Peña <duwanp2112@gmail.com>
2020-07-30 20:41:57 -0430

    Updates

---
+++ precache-manifest.447d529490ca481248a6b0722c5b99f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b900144ef104a0a991824e3c8a31071e",
+    "url": "/Maneja-Presupuesto/index.html"
+  },
+  {
+    "revision": "8108d18bf1c546840339",
+    "url": "/Maneja-Presupuesto/static/css/main.ef334e5f.chunk.css"
+  },
+  {
+    "revision": "110e4dea4e4ebaed951f",
+    "url": "/Maneja-Presupuesto/static/js/2.4d9c31ea.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Maneja-Presupuesto/static/js/2.4d9c31ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8108d18bf1c546840339",
+    "url": "/Maneja-Presupuesto/static/js/main.ce43cdcf.chunk.js"
+  },
+  {
+    "revision": "f177a5d4b8d160fb0c99",
+    "url": "/Maneja-Presupuesto/static/js/runtime-main.60bf97c5.js"
+  }
+]);
